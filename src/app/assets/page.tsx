8f996660c79b9bac9f97525f14f57da00fd8159c
Agustín Longoni <agustinlongoni@gmail.com>
2024-03-22 16:10:40 -0300

    add basic pages for Assets, Settings and Transactions (#17)

---
+++ src/app/assets/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function AssetsPage() {
+  return (
+    <Box pt={4}>
+      <Typography variant="h2">Assets</Typography>
+    </Box>
+  );
+}
+
+AssetsPage.connectedWalletRequired = false;
