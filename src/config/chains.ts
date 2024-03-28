de08f8ac43b7c150ea0879f3f5164b2448763fac
Agustín Longoni <agustinlongoni@gmail.com>
2024-03-28 15:12:05 -0300

    add chainName and fix errors

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,4 +1,5 @@
-export const CHAINS_COLORS = {
+export const CHAINS_COLORS: Record<string, string> = {
   Mainnet: "#00F58C", // unknow id
-  "Testnet Beta 5": "#2b9c80", // 0 id from sdk
+  "Testnet Beta 5": "#F6731D", // 0 id from sdk
+  Unknown: "#cacaca",
 };
