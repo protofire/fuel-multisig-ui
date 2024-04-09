b6ef76121741b3f3020e2460c7b9366f0f089241
Henry Palacios <henrydpalacios@gmail.com>
2024-04-09 18:07:32 -0300

    Working on Tab

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -1,9 +1,12 @@
 import { Box, Typography } from "@mui/material";
 
+import { AssetsTable } from "@/sections/AssetsView/AssetsTable";
+
 export default function AssetsPage() {
   return (
     <Box pt={4}>
       <Typography variant="h2">Assets</Typography>
+      <AssetsTable />
     </Box>
   );
 }
---
+++ src/sections/AssetsView/AssetsTable/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+export const AssetsTable: React.FC = () => {
+  //   const [type, setType] = useState(types[0]);
+  //   const [open, setOpen] = useState(false);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      {/* <AssetTabs options={["Tokens"]}></AssetTabs> */}
+    </Box>
+  );
+};
