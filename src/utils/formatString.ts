6a783d1a9d5eb6a690c4a24fbf426cc7850caec3
0xLucca <0xlucca.dev@gmail.com>
2024-05-14 11:42:19 -0300

    wip: settings logic

--- src/utils/formatString.ts
+++ src/utils/formatString.ts
@@ -88,3 +88,9 @@ export function capitalizeFirstLetter(identifier: string | undefined) {
     ? identifier.charAt(0).toUpperCase() + identifier.slice(1)
     : "";
 }
+
+export function hex_to_bytes(hex: string): number[] {
+  const buffer = Buffer.from(hex.slice(2), "hex");
+  const numbers: number[] = Array.from(buffer);
+  return numbers;
+}
