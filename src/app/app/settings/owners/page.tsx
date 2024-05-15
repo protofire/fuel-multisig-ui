1be4b1f69ddd95079af2f4eae05b93737c2abc18
Henry Palacios <henrydpalacios@gmail.com>
2024-05-15 08:45:38 -0300

    Allowing propose delete user (#56)
    * Allowing propose delete user
    
    * Remove ts error

--- src/app/app/settings/owners/page.tsx
+++ src/app/app/settings/owners/page.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 
+import { AddOwnerStepper } from "@/sections/SettingsView/AddOwnerStepper";
+
 export default function OwnersPage() {
   return (
     <Box display="flex" alignItems="center" flexDirection="column">
-      Owners
+      <AddOwnerStepper />
     </Box>
   );
 }
