af6a552fd85ab127536fa374f6b82b36ce904416
Henry Palacios <henrydpalacios@gmail.com>
2024-05-15 11:36:14 -0300

    Adding global error message in the top

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,8 +3,10 @@ import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 import { useBreadcrumb } from "@/context/BreadcrumbContext";
+import { useInteractionError } from "@/context/InteractionErrorContext/useInteractionError";
 import { Guard } from "@/sections/Auth/Guard";
 import { AppLayout } from "@/sections/layout/AppLayout";
+import ErrorMessage from "@/sections/shared/common/ErrorMessage";
 
 export default function AppDashboardLayout({
   children,
@@ -13,6 +15,7 @@ export default function AppDashboardLayout({
 }) {
   const { addBreadcrumb } = useBreadcrumb();
   const path = usePathname();
+  const { error, setError } = useInteractionError();
 
   useEffect(() => {
     addBreadcrumb(path);
@@ -20,6 +23,10 @@ export default function AppDashboardLayout({
 
   return (
     <Guard connectedWalletRequired={true}>
+      {error?.msg && (
+        <ErrorMessage error={error} clearError={() => setError(null)} />
+      )}
+
       <AppLayout>{children}</AppLayout>
     </Guard>
   );
